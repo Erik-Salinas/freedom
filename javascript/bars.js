59db57225d74a2b1c4330a4c85ec738490351767
Erik Salinas <salinas.erik2002@gmail.com>
2023-12-30 15:37:06 -0300

    Correccion de errores

--- javascript/bars.js
+++ javascript/bars.js
@@ -1,14 +1,25 @@
-//RESPONSIVE
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu__btn');
+    const enlaces = document.querySelector('.menu__enlaces');
+    const barras = document.querySelectorAll('.menu__btn span');
+    const menuItems = document.querySelectorAll('.menu__enlaces .menu-item');
 
-/* const menu__btn = document.querySelector('.menu__btn');
-const enlaces = document.querySelector('.menu__enlaces');
-const barras = document.querySelectorAll('.menu__btn span');
+    menuBtn.addEventListener('click', () => {
+        enlaces.classList.toggle('activado');
+        barras.forEach(child => {child.classList.toggle('animado')});
+        menuBtn.classList.toggle('girar');
+    });
 
+    // Agregar evento de clic a cada elemento del menú de productos
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
 
+            // Cerrar el menú sin afectar las clases
+            enlaces.classList.remove('activado');
+            barras.forEach(child => {child.classList.remove('animado')});
+            menuBtn.classList.remove('girar');
 
-menu__btn.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
-    menu__btn.classList.toggle('girar');
+        });
+    });
+
 });
- */
